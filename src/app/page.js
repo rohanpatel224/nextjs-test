088d806d07b5594139abc3dba8679ce4366749cf
rohanpatel224 <60336647+rohanpatel224@users.noreply.github.com>
2024-06-05 07:13:22 +0000

    nextj test

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function App() {
 	return (
 		<div className='text-center h-full w-full flex items-center justify-center'>
 			<a href={`https://www.nobroker.in/`} className='text-3xl font-semibold underline'>
-				Nobroker.com
+				config change
 			</a>
 		</div>
 	);
